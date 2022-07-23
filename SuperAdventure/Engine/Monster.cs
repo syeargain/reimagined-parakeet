ec7be46ac3d5b0be39a14496fdb675d00bab503d
mmarckel <38478851+mmarckel@users.noreply.github.com>
2022-07-23 10:42:41 -0500

    RPG up to 19.4 or so

---
+++ SuperAdventure/Engine/Monster.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Monster : LivingCreature
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaximumDamage { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+        public List<LootItem> LootTable { get; set; }
+
+        public Monster (int id, string name, int maximumDamage, int rewardExperiencePoints, int rewardGold, int currentHitPoints, int maximumHitPoints)
+            : base(currentHitPoints, maximumHitPoints)
+        {
+            ID = id;
+            Name = name;
+            MaximumDamage = maximumDamage;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+            LootTable = new List<LootItem>();
+        }
+
+        public List<InventoryItem> LootMonster(Monster monster)
+        {
+            // Get random loot items from the monster
+            List<InventoryItem> lootedItems = new List<InventoryItem>();
+
+            // Add items to the lootedItems list, comparing a random number to the drop percentage
+            foreach (LootItem lootItem in monster.LootTable)
+            {
+                if (RandomNumberGenerator.NumberBetween(1, 100) <= lootItem.DropPercentage)
+                {
+                    lootedItems.Add(new InventoryItem(lootItem.Details, 1));
+                }
+            }
+
+            // If no items were randomly selected, then add the default loot item(s).
+            if (lootedItems.Count == 0)
+            {
+                foreach (LootItem lootItem in monster.LootTable)
+                {
+                    if (lootItem.IsDefaultItem)
+                    {
+                        lootedItems.Add(new InventoryItem(lootItem.Details, 1));
+                    }
+                }
+            }
+            return lootedItems;
+        }
+    }
+}
